c982716e670e7332b6e1e5604090121d51b5968a
Vinícius <vinigc19@gmail.com>
2023-05-28 15:38:36 -0300

    Article-Preview - More accessibility & position left Image

--- Article-Preview/Article-Preview/src/app/page.tsx
+++ Article-Preview/Article-Preview/src/app/page.tsx
@@ -17,23 +17,23 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen bg-gray-200">
       <div className="flex w-[58rem] h-[22rem]">
-        <Image style={{objectFit: "cover"}} src={drawers} alt="" className="w-96 rounded-l-2xl" />
+        <Image style={{objectFit: "cover"}} src={drawers} alt="" className="w-96 rounded-l-2xl object-left" />
         <div className="bg-white h-full rounded-r-2xl flex justify-between items-center flex-col p-12">
           <div className="font-bold text-2xl text-gray-600">Shift the overall look and feel by adding these wonderful touches to furniture in your home</div>
           <div className="font-normal text-gray-400">Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting.Ive got some simple tips to help you make any room feel complete.</div>
           <div className="flex justify-between items-center w-full">
             <div className="flex justify-between items-center">
-              <Image src={avatar} alt="" className="h-10 w-10 rounded-3xl" />
+              <Image src={avatar} alt="" className="h-10 w-10 rounded-3xl"/>
               <div className="flex flex-col h-full w-full">
                 <div className="pl-4 text-gray-600 font-bold">Michelle Appleton</div>
                 <div className="pl-4 text-gray-400">28 Jun 2020</div>
               </div>
             </div>
-              <div className="bg-gray-200 rounded-3xl w-10 h-10 flex items-center justify-center pb-1 hover:bg-gray-800"
+              <div className="bg-gray-200 rounded-3xl w-10 h-10 flex items-center justify-center pb-1 hover:bg-gray-800 cursor-pointer"
                 onClick={() => mouseHover()}
               >
                 <Tooltip visible={showTooltip} />
-                <Image src={share} alt="" width={20} height={20}/>
+                <Image src={share} alt="Article" width={20} height={20}/>
               </div>
           </div>
         </div>
--- Article-Preview/Article-Preview/src/app/tooltip.tsx
+++ Article-Preview/Article-Preview/src/app/tooltip.tsx
@@ -19,9 +19,9 @@ const Tooltip: React.FC<TooltipProps> = (props: TooltipProps) => {
                 <div className="text-white font-thin text-sm pr-10">S H A R E</div>
                 <div className="border-solid border-t-[#48556A] border-t-8 border-x-transparent border-x-8 border-b-0 mt-20"></div>
                 <div className="flex justify-between items-center">
-                    <Image className="mr-4" src={facebook} alt='' />
-                    <Image className="mr-4" src={twitter} alt='' />
-                    <Image src={pinterest} alt='' />
+                    <Image className="mr-4 cursor-pointer" src={facebook} alt='' />
+                    <Image className="mr-4 cursor-pointer" src={twitter} alt='' />
+                    <Image className="cursor-pointer" src={pinterest} alt='' />
                 </div>
             </div> : <div></div>
             }
